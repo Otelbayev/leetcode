21420d5812dec7885aae9720da4723d08c1cf85f
Jasurbek <jasurdev1604@gmail.com>
2024-01-22 01:22:24 +0500

    two-nums

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -11,20 +11,27 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  const numIndices = {};
+
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] + nums[j] === target) return [j, i];
+    const complement = target - nums[i];
+
+    if (numIndices.hasOwnProperty(complement)) {
+      return [numIndices[complement], i];
     }
+
+    numIndices[nums[i]] = i;
   }
+
+  return null;
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
 
-let obj = {
-  num: 1,
-};
-console.log(obj.hasOwnProperty("num"));
+// 61/61 cases passed (49 ms)
+// Your runtime beats 94.97 % of javascript submissions
+// Your memory usage beats 5.01 % of javascript submissions (50.4 MB)
 
 // @lc code=end
